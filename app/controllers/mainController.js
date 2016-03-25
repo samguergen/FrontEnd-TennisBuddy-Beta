958b40b4cea24eb126c685af29150af5f1a78f11
Samantha <samguergen@gmail.com>
2016-03-25 17:14:26 -0400

    attempt at geolocation

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -37,5 +37,17 @@ angular.module('myApp', ['uiGmapgoogle-maps'])
     }
   };
 
+var options = {
+                enableHighAccuracy: true
+            };
+
+navigator.geolocation.getCurrentPosition(function(pos) {
+                $scope.position = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+                console.log(JSON.stringify($scope.position));
+            },
+            function(error) {
+                alert('Unable to get location: ' + error.message);
+            }, options);
+
 
 }]);
