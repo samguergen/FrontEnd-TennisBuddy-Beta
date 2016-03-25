a78848ffca397f88cd2e546932d03d2112785197
Samantha <samguergen@gmail.com>
2016-03-24 23:56:18 -0400

    ng controller running

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,12 +1,11 @@
-angular.module('myApp', [])
-.controller(function("$scope"){
+angular.module("myApp", [])
+.controller("mainController", function($scope){
   console.log('inside the controller');
   $scope.user = {
     'firstName': "Miss",
     'lastName': "Nobody",
     'email':"miss@nbd.com",
     'password':"123",
-    games: [];
+    games: []
   };
-
 });
