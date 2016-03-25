553181eb7bff9402b95bb631c7d55ae8f55a2fe2
Samantha <samguergen@gmail.com>
2016-03-25 15:50:58 -0400

    pending

--- app/app.js
+++ app/app.js
@@ -1,7 +1,22 @@
-angular.module('myApp', [])
+// angular.module('myApp', [])
 
-.config(function() {
-})
+// .config(function(uiGmapGoogleMapApiProvider) {
+//     uiGmapGoogleMapApiProvider.configure({
+//         //    key: 'your api key',
+//         v: '3.20', //defaults to latest 3.X anyhow
+//         libraries: 'weather,geometry,visualization'
+//     });
+// })
+
+angular.module('myApp', ['uiGmapgoogle-maps'])
+  .config(
+    ['uiGmapGoogleMapApiProvider', function(GoogleMapApiProviders) {
+        GoogleMapApiProviders.configure({
+            china: true
+        });
+    }]
+)
+
 
 .run(function() {
   console.log('angular app running');
--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,4 +1,41 @@
-angular.module('myApp', [])
-.controller(function(){
+angular.module('myApp', ['uiGmapgoogle-maps'])
+.controller('mainController', ['$scope', 'uiGmapGoogleMapApi', function($scope, uiGmapGoogleMapApi){
   console.log('inside the controller');
-});
+
+  // $scope.uiGmapGoogleMapApi.then(function(maps) {
+  //   console.log(maps);
+  //   });
+
+  $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+
+  $scope.user = {
+    'firstName': "Miss",
+    'lastName': "Nobody",
+    'description': "Some cool hacker chick",
+    'email':"miss@nbd.com",
+    'password':"123",
+    'games': []
+  };
+
+  $scope.tabs = {
+    'home':'active',
+    'profile':'deactivated',
+    'past':'deactivated',
+    'browse':'deactivated',
+    'login':'deactivated'
+  };
+
+  $scope.tabSelection = $scope.tabs[0];
+
+  $scope.tabSwitch = function(tab) {
+    console.log(tab);
+    for (var i in $scope.tabs) {
+      $scope.tabs[i] = 'deactivated';
+      if (i == tab) {
+        $scope.tabs[i] = 'active';
+      }
+    }
+  };
+
+
+}]);
