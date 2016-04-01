2c3dc12c13fd70e53e26a7d5ccd3781e06d7466e
Samantha <samguergen@gmail.com>
2016-03-31 21:04:28 -0400

    test is stage server and added users model

--- server.js
+++ server.js
@@ -1,50 +1,48 @@
 
-// modules =================================================
-var express        = require('express');
-var app            = express();
-var bodyParser     = require('body-parser');
-var methodOverride = require('method-override');
+// // modules =================================================
+// var express        = require('express');
+// var app            = express();
+// var bodyParser     = require('body-parser');
+// var methodOverride = require('method-override');
 
-// configuration ===========================================
+// // configuration ===========================================
 
-// config files
-var db = require('./config/db.js');
+// // config files
+// var db = require('./config/db.js');
 
-// set our port
-var port = process.env.PORT || 8080;
+// // set our port
+// var port = process.env.PORT || 8080;
 
-// connect to our mongoDB database
-// (uncomment after you enter in your own credentials in config/db.js)
-// mongoose.connect(db.url);
+// // connect to our mongoDB database
+// // (uncomment after you enter in your own credentials in config/db.js)
+// // mongoose.connect(db.url);
 
-// get all data/stuff of the body (POST) parameters
-// parse application/json
-app.use(bodyParser.json());
+// // get all data/stuff of the body (POST) parameters
+// // parse application/json
+// app.use(bodyParser.json());
 
-// parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+// // parse application/vnd.api+json as json
+// app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+// // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-// override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(methodOverride('X-HTTP-Method-Override'));
+// // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+// app.use(methodOverride('X-HTTP-Method-Override'));
 
-// set the static files location /public/img will be /img for users
-// app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname));
+// // set the static files location /public/img will be /img for users
+// // app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname));
 
-// routes ==================================================
-require('./app/routes')(app); // configure our routes
+// // routes ==================================================
+// require('./app/routes')(app); // configure our routes
 
-// start app ===============================================
-// startup our app at http://localhost:8080
-app.listen(port);
+// // start app ===============================================
+// // startup our app at http://localhost:8080
+// app.listen(port);
 
-// shoutout to the user
-console.log('Magic happens on port ' + port);
+// // shoutout to the user
+// console.log('Magic happens on port ' + port);
 
-// expose app
-exports = module.exports = app;
-
-var db = require('./config/db');
+// // expose app
+// exports = module.exports = app;
---
+++ test.js
@@ -0,0 +1,23 @@
+var MongoClient = require('mongodb').MongoClient
+
+var URL = 'mongodb://localhost:27017/tennisbuddy'
+
+MongoClient.connect(URL, function(err, db) {
+  if (err) return
+
+  var collection = db.collection('users')
+  collection.insert({
+    firstName: 'sam',
+    lastName: 'guerg',
+    id: 1,
+    cryptId: '1xx',
+    email: 's@g.com,'
+    password: 123123,
+    games: ['sam-vs-marc']
+  }, function(err, result) {
+    collection.find({firstName: 'sam'}).toArray(function(err, docs) {
+      console.log(docs[0])
+      db.close()
+    })
+  })
+})
