32f206358661dc93099c3fef65472b27b7ae35b3
Samantha <samguergen@gmail.com>
2016-03-31 20:52:37 -0400

    route express skeleton for user

---
+++ app/routes.js
@@ -0,0 +1,32 @@
+var User = require('./models/users');
+
+    module.exports = function(app) {
+
+        // server routes ===========================================================
+        // handle things like api calls
+        // authentication routes
+
+        // sample api route
+        app.get('/api/users', function(req, res) {
+            // use mongoose to get all users in the database
+            Nerd.find(function(err, users) {
+
+                // if there is an error retrieving, send the error.
+                                // nothing after res.send(err) will execute
+                if (err)
+                    res.send(err);
+
+                res.json(users); // return all users in JSON format
+            });
+        });
+
+        // route to handle creating goes here (app.post)
+        // route to handle delete goes here (app.delete)
+
+        // frontend routes =========================================================
+        // route to handle all angular requests
+        app.get('*', function(req, res) {
+            res.sendfile('./views/index.html'); // load our public/index.html file
+        });
+
+    };
