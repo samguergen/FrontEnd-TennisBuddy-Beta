1d550711fabb3f9381665ac145a65cbf203a1232
Samantha <samguergen@gmail.com>
2016-04-01 14:57:44 -0400

    angular xeditable correctly loaded

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-angular.module('myApp', ['uiGmapgoogle-maps'])
+angular.module("myApp", ["uiGmapgoogle-maps", "xeditable"])
   .config(
     ['uiGmapGoogleMapApiProvider', function(GoogleMapApiProviders) {
         GoogleMapApiProviders.configure({
@@ -8,6 +8,7 @@ angular.module('myApp', ['uiGmapgoogle-maps'])
 )
 
 
-.run(function() {
+.run(function(editableOptions) {
   console.log('angular app running');
+  editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
 });
